6ab0b68dfea4baadbc9186bd7c4946dc0b1319ab
Long Ha Hoang <long.ha@privadsy.com>
2023-10-16 13:59:42 +0200

    add: recipe details view list ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,14 +10,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Test recipe',
-      'A test recipe',
-      'https://www.foodrepublic.com/img/gallery/the-20-wildest-weirdest-and-most-delicious-recipes-of-the-year/intro-1684945452.jpg'
+      'Tasty Sushi Platter',
+      'A sushi platter recipe',
+      'https://www.foodrepublic.com/img/gallery/the-20-wildest-weirdest-and-most-delicious-recipes-of-the-year/intro-1684945452.jpg',
+      [new Ingredient('Salmon', 1), new Ingredient('Sushi Rice', 20)]
     ),
     new Recipe(
-      'Another Test recipe',
-      'Another test recipe',
-      'https://www.worldhistory.org/img/r/p/1000x1200/6759.jpg.webp?v=1691431569'
+      'Yummy rice bowl',
+      'A yummi rice bowl recipe',
+      'https://www.worldhistory.org/img/r/p/1000x1200/6759.jpg.webp?v=1691431569',
+      [new Ingredient('Sushi Rice', 20), new Ingredient('Meat', 1)]
     ),
   ];
   constructor() {}
