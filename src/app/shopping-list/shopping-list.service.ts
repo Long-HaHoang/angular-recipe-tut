9f7fb491c820bab6de1e00f22f0a0a37624d7630
Long Ha Hoang <long.ha@privadsy.com>
2023-10-16 13:09:05 +0200

    add: shopping update service + EventEmitter

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, OnInit } from '@angular/core';
+import { EventEmitter, Injectable, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShoppingListService implements OnInit {
+  ingriedientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apple', 5),
     new Ingredient('Tomato', 10),
@@ -16,5 +17,10 @@ export class ShoppingListService implements OnInit {
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingriedientsChanged.emit(this.ingredients.slice());
   }
 }
